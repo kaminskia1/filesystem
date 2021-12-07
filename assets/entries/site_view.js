0840618def5d5ac4898b8dfde8ca0823646025fe
kaminskia1 <8168918+kaminskia1@users.noreply.github.com>
2021-12-07 02:13:03 -0500

    added login popup

--- assets/entries/site_view.js
+++ assets/entries/site_view.js
@@ -1,5 +1,33 @@
+const $ = require('jquery');
 const ajax = require('../scripts/ajax');
 const extendTree = require('../scripts/extendTree');
+const Swal = require('sweetalert2');
+
+require('@fortawesome/fontawesome-free/js/all.js');
 
 ajax.ajax();
-extendTree.extendTree(ajax)
+extendTree.extendTree(ajax);
+
+let cache;
+$("#login").click((a) => {
+    if (cache == null) {
+        fetch(a.currentTarget.getAttribute('data-url'))
+            .then(response => response.text())
+            .then(response => {
+                cache = response
+                Swal.fire({
+                    title: 'Login',
+                    html: cache,
+                    showCancelButton: false,
+                    showConfirmButton: false,
+                })
+            })
+    } else {
+        Swal.fire({
+            title: 'Login',
+            html: cache,
+            showCancelButton: false,
+            showConfirmButton: false,
+        })
+    }
+});
