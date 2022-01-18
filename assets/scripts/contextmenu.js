ca59f61de2343878375c4d04f8f3df526920ab4a
kaminskia1 <8168918+kaminskia1@users.noreply.github.com>
2022-01-18 16:49:19 -0500

    refactored context menu

--- assets/scripts/contextmenu.js
+++ assets/scripts/contextmenu.js
@@ -1,144 +1,108 @@
 export function contextmenu(x = []) {
 
-    /** @TODO: refactor actions and add abstractions */
-    let actions = {
-        "folder": {
-            "Rename": {
-                "classes": [],
-                "callback": function (title, baseurl, uuid) {
-                    const Swal = require('sweetalert2');
-                    /** @TODO: figure out how to do proper js routing */
-                    fetch(baseurl + "folder/rename/" + uuid)
-                        .then(response => response.text())
-                        .then(response => {
-                            Swal.fire({
-                                title: title,
-                                html: response,
-                                width: 330,
-                                showCancelButton: false,
-                                showConfirmButton: false,
-                            })
-                        })
-                }
-            },
-            "Move":  {
-                "classes": [],
-                "callback": function (title, baseurl, uuid) {
-                    const Swal = require('sweetalert2');
-                    /** @TODO: figure out how to do proper js routing */
-                    fetch(baseurl + "folder/move/" + uuid)
-                        .then(response => response.text())
-                        .then(response => {
-                            Swal.fire({
-                                title: title,
-                                html: response,
-                                width: 330,
-                                showCancelButton: false,
-                                showConfirmButton: false,
-                            })
-                        })
-                }
-            },
-            "Delete": {
-                "classes": ['danger'],
-                "callback": function (title, baseurl, uuid) {
+    const Swal = require("sweetalert2");
 
-                    const Swal = require('sweetalert2');
-                    const swalWithBootstrapButtons = Swal.mixin({
-                        customClass: {
-                            confirmButton: 'btn btn-success',
-                            cancelButton: 'btn btn-danger'
-                        },
-                        buttonsStyling: false
-                    })
-
-                    swalWithBootstrapButtons.fire({
-                        title: 'Are you sure?',
-                        text: "This action is irreversible!",
-                        icon: 'warning',
-                        showCancelButton: true,
-                        confirmButtonText: 'Yes, delete this.',
-                        cancelButtonText: 'No, Cancel',
-                        reverseButtons: true
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            fetch(baseurl + "folder/delete/" + uuid)
-                                .then(response => response.text())
-                                .then(response => {
-                                    Swal.fire({
-                                        title: title,
-                                        html: response,
-                                        width: 330,
-                                        showCancelButton: false,
-                                        showConfirmButton: false,
-                                    })
-                                })
-
-                        } else if (
-                            /* Read more about handling dismissals below */
-                            result.dismiss === Swal.DismissReason.cancel
-                        ){}
-                    })
-                }
-            }
-        },
-        "file": {}
-    }
-
+    // Grab all elements that desire a contextmenu, if none are described
     if (x.length === 0) x = Array.from(document.querySelectorAll("[data-contextmenu=true]"));
+
+    // Iterate through each element
     x.forEach(element => {
-
-
         if (element.getAttribute("data-contextmenu") === "true"
-            && element.getAttribute("data-contextmenu-binded") !== true
-            && element.getAttribute("data-contextmenu-uuid").length > 0) {
+            && element.getAttribute("data-contextmenu-binded") !== true) {
 
             element.addEventListener('contextmenu', (event) => {
                 event.preventDefault();
 
                 // Remove old instances
                 let all = document.querySelectorAll(".contextmenu")
-                if (all.length > 0)
-                {
-                    all.forEach((a)=>{
-                        a.remove();
+                if (all.length > 0) {
+                    all.forEach((a) => {
+                        a.classList.add("hidden");
                     })
                 }
 
-                // Create container
-                let menu = document.createElement("div");
-                menu.classList.add('contextmenu');
+                // Display current instance
+                let menu = element.querySelector(".contextmenu");
+                menu.classList.remove("hidden")
                 menu.style.left = event.clientX + "px";
                 menu.style.top = event.clientY + "px";
 
-                // Crate list
-                let list = document.createElement("ul");
-                menu.appendChild(list);
+                // Bind menu buttons
+                Array.from(menu.children[0].children).forEach((a)=>{
+                    if (a.getAttribute("data-contextmenu-action-bound") !== "true")
+                    {
+                        switch (a.getAttribute("data-contextmenu-action")) {
+                            case 'popup':
+                                a.addEventListener('click', ()=>{
+                                    fetch(a.getAttribute('data-contextmenu-href'))
+                                        .then(response => response.text())
+                                        .then(response => {
+                                            Swal.fire({
+                                                title: a.getAttribute('data-contextmenu-title'),
+                                                html: response,
+                                                width: 330,
+                                                showCancelButton: false,
+                                                showConfirmButton: false,
+                                            })
+                                        });
+                                });
+                                a.setAttribute("data-contextmenu-action-bound", "true");
+                                break;
 
+                            case 'popup-confirm':
+                                a.addEventListener('click', ()=> {
+                                    const swalWithBootstrapButtons = Swal.mixin({
+                                        customClass: {
+                                            confirmButton: 'btn btn-success',
+                                            cancelButton: 'btn btn-danger'
+                                        },
+                                        buttonsStyling: false
+                                    })
+
+                                    swalWithBootstrapButtons.fire({
+                                        title: 'Are you sure?',
+                                        text: "This action is irreversible!",
+                                        icon: 'warning',
+                                        showCancelButton: true,
+                                        confirmButtonText: 'Yes, Proceed.',
+                                        cancelButtonText: 'No, Cancel',
+                                        reverseButtons: true
+                                    }).then((result) => {
+                                        if (result.isConfirmed) {
+                                            fetch(a.getAttribute('data-contextmenu-href'))
+                                                .then(response => response.text())
+                                                .then(response => {
+                                                    Swal.fire({
+                                                        title: a.getAttribute('data-contextmenu-title'),
+                                                        html: response,
+                                                        width: 330,
+                                                        showCancelButton: false,
+                                                        showConfirmButton: false,
+                                                    })
+                                                })
+
+                                        } else if (
+                                            /* Read more about handling dismissals below */
+                                            result.dismiss === Swal.DismissReason.cancel
+                                        ) {
+                                        }
+                                    });
+                                });
+                                break;
+                        }
+                    }
+                });
+
+
+                // Close menu if non-menu click detected
                 function closer(a) {
                     if (!(a.target === menu || a.target.offsetParent === menu)) {
-                        menu.remove();
+                        menu.classList.add("hidden");
                         document.removeEventListener('click', closer);
                     }
                 }
                 document.addEventListener("click", closer);
 
-                Object.entries(actions[element.getAttribute("data-contextmenu-action")]).forEach(([title, func]) => {
-                    let li = document.createElement("li");
-                    li.innerHTML = title;
-                    func['classes'].forEach((a)=>{
-                        li.classList.add(a);
-                    })
-                    li.addEventListener("click", () => {
-                        func['callback'](title,
-                            element.getAttribute("data-contextmenu-baseurl"),
-                            element.getAttribute("data-contextmenu-uuid")
-                        );
-
-                    })
-                    list.appendChild(li);
-                })
-                document.body.appendChild(menu);
             })
             element.setAttribute("data-contextmenu-binded", true)
         }
